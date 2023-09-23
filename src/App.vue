3aeb28416711f39758fc7bcb0b9aeeb5a5431a6f
Kazuki Arai <zukain5@gmail.com>
2023-09-23 20:02:06 +0900

    create simple example

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,38 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+export interface Item {
+  name: string;
+}
+</script>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      items: [] as Item[],
+    }
+  },
+  computed: {
+    num(){
+      return this.items.length
+    },
+  },
+  methods: {
+    add(){
+      const userName = 'Item' + (this.num + 1);
+      this.items.push({ name: userName });
+    },
+    reset(){
+      this.items = [];
+    },
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <p>Number of items: {{ num }}</p>
+  <button @click="add">Add an item</button>
+  <button @click="reset">Reset</button>
+  <ul>
+    <li v-for="item in items" :key="item.name">{{ item.name }}</li>
+  </ul>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
